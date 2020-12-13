d55f78fa098871016a4a2e8a92d66a9f030c5541
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-12-13 18:25:53 +0100

    Fix FileRead JUnit

--- java_tdd/src/test/java/org/lba/sample/file/txt/FileTxtJUnitExample.java
+++ java_tdd/src/test/java/org/lba/sample/file/txt/FileTxtJUnitExample.java
@@ -1,6 +1,7 @@
 package org.lba.sample.file.txt;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -15,7 +16,7 @@ public class FileTxtJUnitExample {
     @Test
     public void testReadFileWithClassLoader(){
         ClassLoader classLoader = this.getClass().getClassLoader();
-        File file = new File(classLoader.getResource("lorem_ipsum.txt").getFile());
+        File file = new File(classLoader.getResource("lorem_ipsum_txt/lorem_ipsum.txt").getFile());
         assertTrue(file.exists());
  
     }
@@ -30,7 +31,7 @@ public class FileTxtJUnitExample {
     
     @Test
     public void testReadFileWithResource() {
-        URL url = this.getClass().getResource("/lorem_ipsum.txt");
+        URL url = this.getClass().getResource("/lorem_ipsum_txt/lorem_ipsum.txt");
         File file = new File(url.getFile());
         assertTrue(file.exists());
     }
@@ -43,14 +44,20 @@ public class FileTxtJUnitExample {
     
     @Test
     public void readFileRelativePath() {
-        File file = new File("src/test/resources/lorem_ipsum.txt");
+        File file = new File("src/test/resources/lorem_ipsum_txt/lorem_ipsum.txt");
         assertTrue(file.exists());
     }
     
     @Test
     public void testReadAsStream() throws IOException{
-        InputStream is = this.getClass().getResourceAsStream("/lorem_ipsum.txt");
+        InputStream is = this.getClass().getResourceAsStream("/lorem_ipsum_txt/lorem_ipsum.txt");
         assertNotNull(is);
+    }
+    
+    @Test
+    public void testReadAsStreamFileNotExist() throws IOException{
+        InputStream is = this.getClass().getResourceAsStream("/lorem_ipsum.txt");
+        assertNull(is);
     }
     
     @Test
